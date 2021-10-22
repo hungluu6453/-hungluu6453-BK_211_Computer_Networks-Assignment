b968d7cda47132761d485394ac54d3900512d3e5
hungluu6453 <hung.luu.6453@hcmut.edu.vn>
2021-10-22 07:33:01 +0700

    Update recvRtspReply; parseRtspReply; openRtpPort Add parseRtpPacket

--- Client.py
+++ Client.py
@@ -13,12 +13,12 @@ class Client:
 	READY = 1
 	PLAYING = 2
 	state = INIT
-	
+
 	SETUP = 0
 	PLAY = 1
 	PAUSE = 2
 	TEARDOWN = 3
-	
+
 	# Initiation..
 	def __init__(self, master, serveraddr, serverport, rtpport, filename):
 		# Initialize the GUI
@@ -31,14 +31,14 @@ class Client:
 		self.rtpPort = int(rtpport)
 		self.fileName = filename
 		# Parameters
-		self.rtspSeq = 0
+		self.rtspSeq = 0 # CSeq
 		self.sessionId = 0
 		self.requestSent = -1
 		self.teardownAcked = 0
 		self.connectToServer()
-		self.frameNbr = 0
-		
-	# THIS GUI IS JUST FOR REFERENCE ONLY, STUDENTS HAVE TO CREATE THEIR OWN GUI 	
+		self.frameNbr = 0 # seqnum of Rtp packet
+
+	# THIS GUI IS JUST FOR REFERENCE ONLY, STUDENTS HAVE TO CREATE THEIR OWN GUI
 	def createWidgets(self):
 		"""Build GUI."""
 		# Create Setup button
@@ -46,34 +46,35 @@ class Client:
 		self.setup["text"] = "Setup"
 		self.setup["command"] = self.setupMovie
 		self.setup.grid(row=1, column=0, padx=2, pady=2)
-		
-		# Create Play button		
+
+		# Create Play button
 		self.start = Button(self.master, width=20, padx=3, pady=3)
 		self.start["text"] = "Play"
 		self.start["command"] = self.playMovie
 		self.start.grid(row=1, column=1, padx=2, pady=2)
-		
-		# Create Pause button			
+
+		# Create Pause button
 		self.pause = Button(self.master, width=20, padx=3, pady=3)
 		self.pause["text"] = "Pause"
 		self.pause["command"] = self.pauseMovie
 		self.pause.grid(row=1, column=2, padx=2, pady=2)
-		
+
 		# Create Teardown button
 		self.teardown = Button(self.master, width=20, padx=3, pady=3)
 		self.teardown["text"] = "Teardown"
 		self.teardown["command"] =  self.exitClient
 		self.teardown.grid(row=1, column=3, padx=2, pady=2)
-		
+
 		# Create a label to display the movie
 		self.label = Label(self.master, height=19)
-		self.label.grid(row=0, column=0, columnspan=4, sticky=W+E+N+S, padx=5, pady=5) 
+		self.label.grid(row=0, column=0, columnspan=4, sticky=W+E+N+S, padx=5, pady=5)
 
 	# Setup basic operations
+	# Send request -> get response (if the command is PLAY -> there will be responses) -> Display
 	def setupMovie(self):
 		"""Setup button handler."""
 	#TODO
-	
+
 	def exitClient(self):
 		"""Teardown button handler."""
 	#TODO
@@ -81,56 +82,69 @@ class Client:
 	def pauseMovie(self):
 		"""Pause button handler."""
 	#TODO
-	
+
 	def playMovie(self):
 		"""Play button handler."""
 	#TODO
-
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	#
-	def listenRtp(self):		
+	def listenRtp(self):
 		"""Listen for RTP packets."""
 		#TODO
-					
+
 	def writeFrame(self, data):
 		"""Write the received frame to a temp image file. Return the image file."""
 	#TODO
-	
+
 	def updateMovie(self, imageFile):
 		"""Update the image file as video frame in the GUI."""
 	#TODO
-		
+
 	def connectToServer(self):
 		"""Connect to the Server. Start a new RTSP/TCP session."""
+		# self.openRtpPort() ?
+		# self.rtspSocket.connect((self.serverAddr, self.serverPort)) ?
 	#TODO
-	
+
 	def sendRtspRequest(self, requestCode):
-		"""Send RTSP request to the server."""	
+		"""Send RTSP request to the server."""
+		# requestCode is the method name (SETUP,PLAY,...)
+		# Return the response message code
 		#-------------
 		# TO COMPLETE
 		#-------------
-		
-	
-	#
+
+	#  Operations when receive a package
 	def recvRtspReply(self):
-		"""Receive RTSP reply from the server."""
-		#TODO
-	
+		"""Receive RTSP reply from the server.
+			Return the data (response or RtpPacket)"""
+		return self.rtspSocket.recv(256)
+
+	#  Use for response message
 	def parseRtspReply(self, data):
-		"""Parse the RTSP reply from the server."""
-		#TODO
-	
+		"""Parse the RTSP reply from the server.
+			slpit the response into line (an array of line)"""
+		return data.split('\n')
+
+	# Use for Rtp packet (in play)
+	def parseRtpPacket(self, data):
+		""" Decode the Rtp packet
+			Change appropriate parameters
+			Return the video data"""
+		rtpPacket = RtpPacket()
+		rtpPacket.decode(data)
+		self.frameNbr = rtpPacket.seqNum()
+
+		return rtpPacket.getPayload()
+
+	#  Initialize the socket
 	def openRtpPort(self):
 		"""Open RTP socket binded to a specified port."""
-		#-------------
-		# TO COMPLETE
-		#-------------
-		# Create a new datagram socket to receive RTP packets from the server
-		# self.rtpSocket = ...
-		
-		# Set the timeout value of the socket to 0.5sec
-		# ...
-		
+		self.rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.rtspSocket.settimeout(0.5)
 
+
+	#  Additional handler
 	def handler(self):
 		"""Handler on explicitly closing the GUI window."""
 		#TODO
